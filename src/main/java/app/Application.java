7e4e58e231aab4bc5be329ea5ac303bdbdad6601
Jonathan Battisti <jonathan.battisti.1@gmail.com>
2018-07-16 20:20:17 -0400

    MAJ test for Application

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -1,5 +1,10 @@
 package app;
 
+import java.io.File;
+
 public class Application {
+	public static void main(String[] args) {
+		
+	}
 
 }
--- src/test/java/app/ApplicationTest.java
+++ src/test/java/app/ApplicationTest.java
@@ -1,12 +1,19 @@
 package app;
 
+import static org.junit.Assert.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.File;
+
 import org.junit.jupiter.api.Test;
 
 class ApplicationTest {
 
 	@Test
-	void testNothing() {
+	void ApplicationTest() {
+		String[] paramTbl = {"UPDATE test SET nom = B where nom = C","excel.csv"};
+		Application.main(paramTbl);
+		assertTrue(new File("script.sql").exists());
 	}
 
 }
